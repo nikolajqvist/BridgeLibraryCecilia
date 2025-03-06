4b6a0e0369ad1b66208ae6d2b5db086d40c392f4
Cecilia Liv Starck <cecilia-liv@hotmail.com>
2025-03-06 13:26:09 +0100

    opg

---
+++ StoreBaeltTicketLib/StoreBaeltCar.cs
@@ -0,0 +1,19 @@
+﻿using BridgeLibrary;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace StoreBaeltTicketLib
+{
+    public class StoreBaeltCar : Car
+    {
+        public override double Price()
+        {
+            double price = base.Price();
+            if (Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                price *= 0.85;
+            }
+
+            return price;
+        }
+    }
+}
